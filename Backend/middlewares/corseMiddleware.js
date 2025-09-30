442e28f635c027edd0e6a1d270ba42b72584ae52
S-Q-Ali <syedqasim963@gmail.com>
2025-09-30 13:38:55 -0400

    origin set to local and vercel url

--- Backend/middlewares/corseMiddleware.js
+++ Backend/middlewares/corseMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: "*",
+  origin: ["https://otp-extractor.vercel.app/","http://localhost:5173/"],
 });
 
 module.exports = corsMiddleware;
