7a93e3908aea866162b01240067a2c18ee226f57
S-Q-Ali <syedqasim963@gmail.com>
2025-09-30 07:06:32 -0400

    updated and added the sharedKeyAuth middleware, first it will checks the keys if matched then other routers will work else will show a error message

--- Backend/routes/ghlRoute.js
+++ Backend/routes/ghlRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getGhlOtp } = require("../controllers/ghlController");
+const sharedKeyAuth=require("../middlewares/sharedKeyAuth")
 
 // POST /get-ghl-otp
-router.post("/get-ghl-otp", getGhlOtp);
+router.post("/get-ghl-otp",sharedKeyAuth, getGhlOtp);
 
 module.exports = router;
