5709fde7dc9fedb57b7b7fd56d8c456515ac3e66
S-Q-Ali <syedqasim963@gmail.com>
2025-09-30 14:03:22 -0400

    updated

--- Backend/middlewares/corseMiddleware.js
+++ Backend/middlewares/corseMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: ["https://otp-extractor.vercel.app/","http://localhost:5173/"],
+  origin: ["https://otp-extractor.vercel.app","http://localhost:5173/"],
 });
 
 module.exports = corsMiddleware;
